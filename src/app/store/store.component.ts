9e1919b1a42ae4c031dd998dc0a2d9f182f5e902
vronney <ronneyvargas@gmail.com>
2020-08-12 10:45:03 -0500

    Added scrollToTop for page buttons, fixed to top category menu

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -47,4 +47,9 @@ export class StoreComponent implements OnInit {
       .fill(0).map((x, i) => i + 1);
   }
 
+  scrollToTop(): void {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
 }
